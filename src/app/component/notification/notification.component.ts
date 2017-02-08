1960398f2c71683124f734aac4a934dee44c31c1
Lagniez <jmclagniez@gmail.com>
2017-02-08 10:27:23 +0100

    list notification

--- src/app/component/notification/notification.component.ts
+++ src/app/component/notification/notification.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Notification} from '../../model/notification';
+import { Component, OnInit, Input} from '@angular/core';
+import { Notification } from '../../model/notification';
 
 @Component({
   selector: 'app-notification',
@@ -7,14 +7,14 @@ import {Notification} from '../../model/notification';
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
+
+	@Input()
   notification: Notification = new Notification();
 
   constructor() { }
 
-  ngOnInit() {
-     this.notification.name = "name";
-     this.notification.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. ";
-     this.notification.image = new Image().src = "http://www.w3schools.com/css/trolltunga.jpg";
+  ngOnInit() { 
+     
   }
 
 }
